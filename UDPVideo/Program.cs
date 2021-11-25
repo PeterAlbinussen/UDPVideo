f025148d250802455646a70c46f7360a99c63486
Peter Albinussen <pete47m0@edu.easj.dk>
2021-11-25 09:36:48 +0100

    Add project files.

---
+++ UDPVideo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDPVideo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
